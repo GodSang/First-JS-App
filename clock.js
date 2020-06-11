55591405af2509b7ed0afb8b837015754cf6880a
God Sang <tkdgh583@naver.com>
2020-06-11 17:19:06 +0900

    Part 1. Make Clock

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  // 첫번째 인자는 함수, 두번째 인자는 실행할 시간 간격(밀리 세컨드 단위)
+  setInterval(getTime, 1000);
+}
+
+init();
